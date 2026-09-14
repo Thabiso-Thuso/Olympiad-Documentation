199d41283826ccb8d3ab855e407c89220f57cb49
Thabiso <ramaemathuso@gmail.com>
2026-09-14 23:49:25 +0200

    Merged config changes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,16 +20,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://thabiso-thuso.github.io',  
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Olympiad-Documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'Thabiso-Thuso', // Usually your GitHub org/user name.
+  projectName: 'Olympiad-Documentation', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
@@ -78,6 +78,7 @@ const config: Config = {
           position: 'left',
           label: 'Documentation',
         },
+        
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -117,6 +118,7 @@ const config: Config = {
         {
           title: 'More',
           items: [
+     
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
